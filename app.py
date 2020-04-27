49ba80facb2ffc67bc5c054d66ace8b08c7c32f7
Tianhua Chen <1159184195@qq.com>
2020-04-28 00:55:06 +0300

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from  flask  import  Flask , request
-from  flask_restful  import  Resource,  Api
+from  flask_restful  import  Resource,  Api , reqparse
+from  flask_jwt  import JWT,   jwt_required
+
+from  security  import   authenticate,  identity
 
 
 app  =   Flask(__name__)
 app.secret_key = 'Tianhua'
 api   =  Api(app)
-
 
 
 items  =  [ ]
@@ -23,9 +25,29 @@ class  Item(Resource) :
                         items.append(item)
                         return  item,        201  
 
+
+    def  put  ( self,    name ):
+              parser  =  reqparse .  RequestParser (  )
+              parser . add_argument  ( ' price'  ,
+                    type  =  float  ,
+                    required  =  True ,
+                    help  =  'This  field  cannot  be  left  blank!'
+               )
+              data  =  parser . parse_args (  )
+              print ( data['another']) ;
+              item  =  next(filter (lambda x:    x['name']  ==   name,  items ),   None)
+              if   item  is   None : 
+                    item  =  {'name':  name,   'price':   data['price']}
+                    items . append(item)
+              else :
+                    item . update (data)
+              return  item
+
 class ItemList(Resource) :
     def  get(self):
                         return{'items' : items}
+
+
 
 api . add_resource ( Item,  '/item/<string:name>')
 api . add_resource (ItemList,  '/items')
